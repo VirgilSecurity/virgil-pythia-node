811301af770684e0a1f5d01c449ad5e042ebbd7e
Vadim Avdeiev <v.avdeiev@gmail.com>
2018-04-26 19:36:15 +0300

    add PythiaClient tests

--- src/client/PythiaClient.js
+++ src/client/PythiaClient.js
@@ -1,19 +1,19 @@
-import { request } from 'https';
 import { Connection } from './Connection';
 
 export class PythiaClient {
-	constructor(apiUrl, appId) {
-		if (typeof apiUrl === 'string') {
+
+	constructor(apiUrl) {
+		if (apiUrl == null) {
+			this.connection = new Connection('https://api.virgilsecurity.com');
+		} else if (typeof apiUrl === 'string') {
 			this.connection = new Connection(apiUrl);
 		} else {
-			this.connection = new Connection('https://api.virgilsecurity.com');
+			this.connection = apiUrl;
 		}
-
-		this.appId = appId;
 	}
 
-	async transformPassword (params) {
-		const { salt, blindedPassword, version, token, includeProof } = params;
+	transformPassword (params) {
+		const { salt, blindedPassword, token, version, includeProof } = params;
 		const body = {
 			blinded_password: blindedPassword.toString('base64'),
 			user_id: salt.toString('base64')
@@ -27,31 +27,32 @@ export class PythiaClient {
 			body.include_proof = includeProof;
 		}
 
-		const headers = {
-			'X-Application-Id': this.appId
-		};
-
-		const response = await this.connection.send(
+		return this.connection.send(
 			'/pythia/v1/password',
 			{
-				headers,
 				body,
 				accessToken: token
 			}
-		);
-
-		if (!response.ok) {
-			// TODO throw custom error
-			throw new Error(`HttpError: ${resposne.status} - ${resposne.statusText}`);
-		}
-
-		const { transformed_password, proof } = await response.json();
-		return {
-			transformedPassword: Buffer.from(transformed_password, 'base64'),
-			proof: {
-				valueC: Buffer.from(proof.value_c, 'base64'),
-				valueU: Buffer.from(proof.value_u, 'base64')
+		).then(response => {
+			if (!response.ok) {
+				// TODO throw custom error
+				throw new Error(`HttpError: ${response.status} - ${response.statusText}`);
 			}
-		};
+
+			return response.json();
+		}).then(({ transformed_password, proof }) => {
+			const result = {
+				transformedPassword: Buffer.from(transformed_password, 'base64')
+			};
+
+			if (includeProof) {
+				result.proof = {
+					valueC: Buffer.from(proof.value_c, 'base64'),
+					valueU: Buffer.from(proof.value_u, 'base64')
+				};
+			}
+
+			return result;
+		});
 	}
 }
