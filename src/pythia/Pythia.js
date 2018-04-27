5f90f35d70ae48436d6193c07cebdc08c17e3f2f
Vadim Avdeiev <v.avdeiev@gmail.com>
2018-04-27 18:14:26 +0300

    implement Pythia#register

--- src/pythia/Pythia.js
+++ src/pythia/Pythia.js
@@ -1,3 +1,6 @@
+import { blind, deblind, generateSalt, verify } from '../crypto';
+import { PythiaUser } from './PythiaUser';
+
 export class Pythia {
 	constructor(params) {
 		const { proofKeys, accessTokenProvider, client } = params;
@@ -11,7 +14,36 @@ export class Pythia {
 	}
 
 	register(password) {
-		throw new Error('Not implemented');
+		const salt = generateSalt();
+		const { blindedPassword, blindingSecret } = blind(password);
+		const latestProofKey = this.proofKeys.currentKey();
+
+		const tokenContext = { service: 'pythia', operation: 'transform', forceReload: false };
+		return this.accessTokenProvider.getToken(tokenContext).then(accessToken =>
+			this.client.transformPassword({
+				blindedPassword,
+				salt,
+				version: latestProofKey.version,
+				includeProof: true,
+				token: accessToken.toString()
+			})
+		).then(({ transformedPassword, proof }) => {
+			const verified = verify(
+				transformedPassword,
+				blindedPassword,
+				salt,
+				latestProofKey.key,
+				proof.valueC,
+				proof.valueU
+			);
+
+			if (!verified) {
+				throw new Error('Transformed password verification has failed');
+			}
+
+			const deblindedPassword = deblind(transformedPassword, blindingSecret);
+			return new PythiaUser(salt, deblindedPassword, latestProofKey.version);
+		});
 	}
 
 	updateUser(updateToken, pythiaUser) {
