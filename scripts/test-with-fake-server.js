445059a4e1f503bb3897f3548fd4c62463b52dbb
Vadim Avdeiev <v.avdeiev@gmail.com>
2018-05-04 15:15:13 +0300

    functional test

--- scripts/test-with-fake-server.js
+++ scripts/test-with-fake-server.js
@@ -2,41 +2,60 @@ require('babel-register');
 require('dotenv').config();
 
 const { createPythia } = require('../src/pythia/createPythia');
+const { ProofKeys } = require('../src/pythia/ProofKeys');
+
+const proofKeyList = process.env.MY_PROOF_KEYS.split(';');
 
 const pythia = createPythia({
 	apiKeyBase64: process.env.VIRGIL_API_KEY,
 	apiKeyId: process.env.VIRGIL_API_KEY_ID,
 	appId: process.env.VIRGIL_APP_ID,
-	proofKeys: process.env.MY_PROOF_KEYS,
+	proofKeys: proofKeyList[0],
 	apiUrl: process.env.VIRGIL_API_URL
 });
 
+const updateToken = process.env.MY_UPDATE_TOKEN;
+
 const thePassword = 'my password';
 
-pythia.register(thePassword)
-	.then(user => {
+pythia.createBreachProofPassword(thePassword)
+	.then(bpPassword => {
 		console.log('Registered');
-		console.log(JSON.stringify(user));
-		return user;
+		console.log(JSON.stringify(bpPassword));
+		return bpPassword;
 	})
-	.then(user => {
-		console.log('Trying to authenticate with wrong password');
-		return pythia.authenticate('wrong password', user, false)
-			.then(authenticated => {
-				if (authenticated) {
-					throw new Error('Authenticated with a wrong password');
-				}
-
-				console.log('Now the right password');
-				return pythia.authenticate(thePassword, user, false);
-			})
-			.then(authenticated => {
-				if (authenticated) {
-					console.log('Welcome, %username%!');
-					return;
-				}
-
-				throw new Error('Authentication failed with correct password');
-			});
-	})
+	.then(bpPassword =>  testAuthentication(bpPassword))
+	.then(bpPassword => testUpdate(bpPassword))
+	.then(updatedBpPassword => testAuthentication(updatedBpPassword))
 	.catch(err => console.log(err));
+
+function testAuthentication(bpPassword) {
+	console.log('Trying to authenticate with wrong password');
+	return pythia.verifyBreachProofPassword('wrong password', bpPassword, false)
+		.then(authenticated => {
+			if (authenticated) {
+				throw new Error('Authenticated with a wrong password');
+			}
+
+			console.log('Now the right password');
+			return pythia.verifyBreachProofPassword(thePassword, bpPassword, true);
+		})
+		.then(authenticated => {
+			if (!authenticated) {
+				throw new Error('Authentication failed with correct password');
+			}
+
+			console.log('Welcome, %username%!');
+			return bpPassword;
+		});
+}
+
+function testUpdate(bpPassword) {
+	console.log('setting new proof key');
+	pythia.proofKeys = new ProofKeys(proofKeyList);
+	console.log('Updating the breach-proof password');
+	const newBpPassword = pythia.updateBreachProofPassword(updateToken, bpPassword);
+	console.log('New bpPassword');
+	console.log(JSON.stringify(newBpPassword));
+	return newBpPassword;
+}
