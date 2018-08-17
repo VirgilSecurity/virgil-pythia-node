cea5ac86801f169154641622cc991417d5a67645
Vadim Avdeiev <v.avdeiev@gmail.com>
2018-08-17 19:42:03 +0300

    stop bundling dependencies in node.js builds

--- config/rollup.config.es.js
+++ config/rollup.config.es.js
@@ -1,13 +1,9 @@
 import config from './rollup.config';
-import builtinModules from 'builtin-modules';
 
 export default config({
 	output: {
 		file: 'dist/virgil-pythia.es.js',
 		format: 'es'
 	},
-	isBrowser: false,
-	external: (id) => {
-		return builtinModules.indexOf(id) > -1 || (id.length > 5 && id.slice(-5) === '.node');
-	}
+	isBrowser: false
 })
