fe3a313cea76f545bb4e490b8dbe679c1bcb0de2
Serhii Nanovskyi <serhii.nanovskyi@icloud.com>
2019-08-20 16:48:03 +0300

    Add build steps

--- src/initPythia.ts
+++ src/initPythia.ts
@@ -2,7 +2,7 @@ import initPythiaModules from '@virgilsecurity/core-pythia';
 
 import { setPythiaModules } from './pythiaModules';
 
-export const initPythia = async () => {
+export async function initPythia() {
   const pythiaModules = await initPythiaModules();
   setPythiaModules(pythiaModules);
-};
+}
