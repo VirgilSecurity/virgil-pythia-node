fc5cadb7ede50b55fd488160deaea7b5b1bc190a
Serhii Nanovskyi <serhii.nanovskyi@icloud.com>
2019-09-06 17:51:38 +0300

    Fix linter errors

--- src/errors.ts
+++ src/errors.ts
@@ -10,29 +10,25 @@ export class PythiaClientError extends PythiaError {
   readonly code: number;
   readonly httpStatus: number;
 
-	constructor(message: string, code: number, httpStatus: number) {
-		super(message);
-		this.code = code;
-		this.httpStatus = httpStatus;
-	}
+  constructor(message: string, code: number, httpStatus: number) {
+    super(message);
+    this.code = code;
+    this.httpStatus = httpStatus;
+  }
 }
 
 export class ProofVerificationFailedError extends PythiaError {
-	constructor() {
-		super('Transformed password proof verification has failed');
-		this.name = 'ProofVerificationFailedError';
-	}
+  constructor() {
+    super('Transformed password proof verification has failed');
+    this.name = 'ProofVerificationFailedError';
+  }
 }
 
 export class UnexpectedBreachProofPasswordVersionError extends PythiaError {
-	constructor(expectedVersion: number, actualVersion: number) {
-		super(
-			`Unexpected Breach-proof password version. Expected ${
-				expectedVersion
-			}, got ${
-				actualVersion
-			}`
-		);
-		this.name = 'UnexpectedBreachProofPasswordVersionError';
-	}
+  constructor(expectedVersion: number, actualVersion: number) {
+    super(
+      `Unexpected Breach-proof password version. Expected ${expectedVersion}, got ${actualVersion}`,
+    );
+    this.name = 'UnexpectedBreachProofPasswordVersionError';
+  }
 }
