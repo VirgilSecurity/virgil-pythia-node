e0a367c9119a92634d9fcff4f327584f9d8132d6
Vadim Avdeev <v.avdeiev@gmail.com>
2019-09-06 19:55:19 +0300

    fix custom error classes for react-native

--- src/errors.ts
+++ src/errors.ts
@@ -6,29 +6,33 @@ export class PythiaError extends Error {
   }
 }
 
-export class PythiaClientError extends PythiaError {
+export class PythiaClientError extends Error {
   readonly code?: number;
   readonly httpStatus?: number;
 
   constructor(message: string, code?: number, httpStatus?: number) {
     super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
+    this.name = 'PythiaClientError';
     this.code = code;
     this.httpStatus = httpStatus;
   }
 }
 
-export class ProofVerificationFailedError extends PythiaError {
+export class ProofVerificationFailedError extends Error {
   constructor() {
     super('Transformed password proof verification has failed');
+    Object.setPrototypeOf(this, new.target.prototype);
     this.name = 'ProofVerificationFailedError';
   }
 }
 
-export class UnexpectedBreachProofPasswordVersionError extends PythiaError {
+export class UnexpectedBreachProofPasswordVersionError extends Error {
   constructor(expectedVersion: number, actualVersion: number) {
     super(
       `Unexpected Breach-proof password version. Expected ${expectedVersion}, got ${actualVersion}`,
     );
+    Object.setPrototypeOf(this, new.target.prototype);
     this.name = 'UnexpectedBreachProofPasswordVersionError';
   }
 }
