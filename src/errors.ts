c1f6282c21a593cd637faec9c16fe1846451d836
Serhii Nanovskyi <snanovskyi@icloud.com>
2019-10-15 15:13:43 +0300

    Fix custom errors in React Native

--- src/errors.ts
+++ src/errors.ts
@@ -1,38 +1,38 @@
 export class PythiaError extends Error {
-  constructor(message: string) {
+  constructor(message: string, name: string = 'PythiaError', ParentClass: any = PythiaError) {
     super(message);
-    Object.setPrototypeOf(this, new.target.prototype);
-    this.name = 'PythiaError';
+    Object.setPrototypeOf(this, ParentClass.prototype);
+    this.name = name;
   }
 }
 
-export class PythiaClientError extends Error {
+export class PythiaClientError extends PythiaError {
   readonly code?: number;
   readonly httpStatus?: number;
 
   constructor(message: string, code?: number, httpStatus?: number) {
-    super(message);
-    Object.setPrototypeOf(this, new.target.prototype);
-    this.name = 'PythiaClientError';
+    super(message, 'PythiaClientError', PythiaClientError);
     this.code = code;
     this.httpStatus = httpStatus;
   }
 }
 
-export class ProofVerificationFailedError extends Error {
+export class ProofVerificationFailedError extends PythiaError {
   constructor() {
-    super('Transformed password proof verification has failed');
-    Object.setPrototypeOf(this, new.target.prototype);
-    this.name = 'ProofVerificationFailedError';
+    super(
+      'Transformed password proof verification has failed',
+      'ProofVerificationFailedError',
+      ProofVerificationFailedError,
+    );
   }
 }
 
-export class UnexpectedBreachProofPasswordVersionError extends Error {
+export class UnexpectedBreachProofPasswordVersionError extends PythiaError {
   constructor(expectedVersion: number, actualVersion: number) {
     super(
       `Unexpected Breach-proof password version. Expected ${expectedVersion}, got ${actualVersion}`,
+      'UnexpectedBreachProofPasswordVersionError',
+      UnexpectedBreachProofPasswordVersionError,
     );
-    Object.setPrototypeOf(this, new.target.prototype);
-    this.name = 'UnexpectedBreachProofPasswordVersionError';
   }
 }
